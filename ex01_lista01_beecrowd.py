54e92ce4ce79de3473c3350da670ce6c697ff325
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2024-12-05 19:39:13 -0300

    finished first beecrowd task

--- ex01_lista01_beecrowd.py
+++ ex01_lista01_beecrowd.py
@@ -46,6 +46,106 @@ class DoublyCircularLinkedList:
         self.size -= 1 # decrementa o contador de candidatos
 
         return node.data # retorna a informação do nó removido
-    
-    def __str__(self) -> str:
-        
+
+class InputNode:
+    def __init__(self, n, k, m):
+        self.n = n # número de participantes
+        self.k = k # passos no sentido horário
+        self.m = m # passos no sentido anti-horário 
+
+        self.next = None # ponteiro para o próximo nó
+
+def josephus_simulation(n, k, m):
+    participants = DoublyCircularLinkedList() # inicializa a lista circular
+
+    for i in range(n):
+        participants.add(i + 1) # adiciona os números na lista
+
+    clockwise_pointer = participants.head # aponta para o primeiro elemento e vai girar no sentido horário (k)
+    counterclockwise_pointer = participants.head.prev # aponta para o último e gira no sentido anti-horário (m)
+
+    result = '' # armazenará a saída formatada
+
+    while participants.size > 0:
+        for _ in range(k - 1):
+            clockwise_pointer = clockwise_pointer.next # aponta para o candidato que será retirado pela direita
+
+        for _ in range(m - 1):
+            counterclockwise_pointer = counterclockwise_pointer.prev # aponta para o que será retirado pela esquerda
+
+        if clockwise_pointer == counterclockwise_pointer: # se o mesmo candidato for escolhido por ambos
+            chosen = participants.remove(clockwise_pointer) # retira apenas ele
+
+            result += f'{chosen:3}' # adiciona o número do candidato removido à saída
+
+            clockwise_pointer = clockwise_pointer.next # aponta para o próximo para recomeçar a contagem
+            counterclockwise_pointer = counterclockwise_pointer.prev # aponta para o próximo
+
+        else:
+            chosen1 = participants.remove(clockwise_pointer) # remove o primeiro candidato e armazena seu número
+            chosen2 = participants.remove(counterclockwise_pointer)# faz o mesmo com o segundo candidato
+
+            result += f'{chosen1:3}{chosen2:3}' # adiciona os números dos candidatos à saída
+
+            clockwise_pointer = clockwise_pointer.next # aponta para o próximo no sentido horário
+            counterclockwise_pointer = counterclockwise_pointer.prev # aponta para o próximo no anti-horário
+
+        if participants.size > 0:
+            result += ', ' # adiciona uma vírgula e um espaço, exceto no final
+
+    return result # retorna a sequência formatada final
+
+def parse_input():
+    head = None # ponteiro para o início da lista encadeada
+    tail = None # ponteiro para o final
+
+    control = True # variável de controle para o laço
+
+    while control:
+        line = input() # recebe a entrada
+
+        if line == '0 0 0':
+            control = False # termina quando a '0 0 0' for inserido como entrada
+
+        else: # obtém os números
+            n = m = k = ''
+            idx = 0
+
+            while idx < len(line) and line[idx] != ' ':
+                n += line[idx]
+                idx += 1
+            idx += 1
+            while idx < len(line) and line[idx] != ' ':
+                k += line[idx]
+                idx += 1
+            idx += 1
+            while idx < len(line):
+                m += line[idx]
+                idx += 1
+
+            n, k, m = int(n), int(k), int(m)
+            new_node = InputNode(n, k, m) # cria um novo nó com as entradas
+
+            if head is None: # se for o primeiro nó, ajusta os ponteiros de acordo
+                head = new_node
+                tail = new_node
+
+            else: # se não for, ajusta apenas o ponteiro do final
+                tail.next = new_node
+                tail = new_node
+
+    return head # retorna o início da lista encadeada
+
+def main():
+    inputs = parse_input()
+
+    current = inputs
+
+    while current is not None:
+        result = josephus_simulation(current.n, current.k, current.m)
+        print(result)
+        current = current.next
+
+
+if __name__ == '__main__':
+    main()
